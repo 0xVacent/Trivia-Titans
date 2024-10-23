1625c3745adf3f18be7b44828b2f7e9703d6d6f7
matidepa <matidepa22@hotmail.com>
2024-10-23 11:46:27 -0300

    EndGameFixed

--- src/app/shared/end-game/end-game.component.ts
+++ src/app/shared/end-game/end-game.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-end-game',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './end-game.component.html',
   styleUrl: './end-game.component.css'
 })
